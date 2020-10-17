df02f350e7117a9a35879f739161f92d48c5c194
struy <struykm@yandex.ru>
2020-10-17 16:09:32 -0500

    module 1

--- README.md
+++ README.md
@@ -11,3 +11,8 @@ Build programming portfolio
 ## Module 1
 
 Automatically resizes and rotates a bunch of images, and converts them from one image format to another.
+
+> curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=$11hg55-dKdHN63yJP20dMLAgPJ5oiTOHF" > /dev/null | curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=11hg55-dKdHN63yJP20dMLAgPJ5oiTOHF" -o images.zip && sudo rm -rf cookie
+
+> chmod +x <script_name>.py
+> 
---
+++ module_1/fix_design.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+
+import os
+from PIL import Image
+
+
+SIZE = (128, 128,)
+FROM = os.getcwd() + "/images/"
+# origin path TO = "/opt/icons/"
+TO = os.getcwd() + "/img2/"
+
+
+for filename in os.listdir(FROM):
+    # skip Apple system file
+    if filename == '.DS_Store':
+        continue
+    #  Open an image
+    with Image.open(FROM + filename) as im:
+        # Rotate an image and   Resize an image
+        new_im = im.rotate(-90).resize(SIZE)
+        # Save an image in a specific format in a separate directory
+        if new_im.mode != 'RGB':
+            new_im = new_im.convert('RGB')
+        new_im.save(TO+filename+".jpg", "jpeg")
+        new_im.close()
