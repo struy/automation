2880a0d0fc1fd189ff279169d63638338f08bbc1
struy <struykm@yandex.ru>
2020-10-17 21:07:27 -0500

    start module 3

---
+++ module_3/emails.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import email.message
+import mimetypes
+import os.path
+import smtplib
+
+def generate(*args):
+    pass
+
+def send(message):
+    pass
---
+++ module_3/reports.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer, Table, Image
+
+
+def generate(*args):
+    pass
+
